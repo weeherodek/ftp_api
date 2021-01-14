a2828daa43c02cf4d233f4016b51dc30e9d18fad
unknown <herodek_10@hotmail.com>
2021-01-14 15:29:32 -0300

    Adding cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 const routes = require('./routes');
 const Schedule = require('../FTP_API/helper/schedule');
@@ -8,6 +9,10 @@ const port  = process.env.PORT || 3003;
 
 Schedule.updateImage();
 Schedule.updateGif();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 app.use(routes);
 
