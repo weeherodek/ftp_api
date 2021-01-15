fd116805fa0a9ca0802d76e12b56b084fa8b617e
unknown <herodek_10@hotmail.com>
2021-01-15 15:52:35 -0300

    Alterando rota git

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,7 @@ router.get('/images', MainController.getImages);
 router.get('/getgif', MainController.getGif);
 router.get('/lastimage', MainController.lastImage);
 router.get('/app/ftp/intermediate-octree.gif', (req,res)=>{
-    res.send(path.join(__dirname,'ftp','intermediate-neuquant.gif'))
+    res.send(path.join(__dirname,'ftp','intermediate-octree.gif'))
 })
 
 module.exports = router;
